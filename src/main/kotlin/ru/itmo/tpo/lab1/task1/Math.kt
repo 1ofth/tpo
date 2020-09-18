503b40ecfd7e05261e8ce8ea0c5b558d84cbeaf6
1ofth <edem.ibraimov.2000@mail.ru>
2020-09-18 17:59:56 +0300

    Task1: cos implementation using BigDecimal.
    Works well with |x| around 1000.
    Also can compute cos for |x| around 10000, but it takes much more time, about 30 seconds.

--- src/main/kotlin/ru/itmo/tpo/lab1/task1/Math.kt
+++ src/main/kotlin/ru/itmo/tpo/lab1/task1/Math.kt
@@ -1,14 +1,52 @@
 package ru.itmo.tpo.lab1.task1
 
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.math.RoundingMode
 import kotlin.math.abs
 import kotlin.math.pow
 
 fun main() {
     println(Math().cos(-.1, 0.000001))
+
+    println(Math().cos((-.1).toBigDecimal(), 0.000001.toBigDecimal()))
+    println(Math().cos(1.0.toBigDecimal(), 0.000001.toBigDecimal()))
+    println(Math().cos(10.0.toBigDecimal(), 0.000001.toBigDecimal()))
+    println(Math().cos(100.0.toBigDecimal(), 0.000001.toBigDecimal()))
+    println(Math().cos(1000.0.toBigDecimal(), 0.000001.toBigDecimal()))
+    println(Math().cos(10000.0.toBigDecimal(), 0.000001.toBigDecimal()))
 }
 
 class Math {
     fun Int.sign(): Int = (-1).toDouble().pow(this).toInt()
+
+    fun cos(param: BigDecimal, precision: BigDecimal): Double {
+        var prevValue: BigDecimal
+        var currentValue = BigDecimal.valueOf(1)
+        var iteration = 0
+        var factor: BigInteger = BigInteger.ONE
+        var diff = BigDecimal.valueOf(1)
+        var numerator = BigDecimal.ONE
+
+        while (iteration < MIN_ITERATIONS ||
+            (diff.abs() > precision) && iteration < MAX_BIG_ITERATIONS
+        ) {
+            iteration += 1
+
+            factor *= BigInteger.valueOf(iteration * 2L - 1) * BigInteger.valueOf(iteration * 2L)
+
+            prevValue = currentValue
+
+            numerator *= param * param
+            numerator = numerator.negate()
+
+            currentValue = currentValue.add(
+                (numerator.divide(factor.toBigDecimal(), 10, RoundingMode.HALF_UP))
+            )
+            diff = currentValue - prevValue
+        }
+        return currentValue.toDouble()
+    }
 
     fun cos(param: Double, precision: Double): Double {
         var prevValue = Double.MIN_VALUE
@@ -32,5 +70,6 @@ class Math {
     companion object {
         const val MIN_ITERATIONS = 5
         const val MAX_ITERATIONS = 10
+        const val MAX_BIG_ITERATIONS = 15000
     }
 }
