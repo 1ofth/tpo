98c747f53ac1f3ac09ca97e546c930c31b8edc55
1ofth <edem.ibraimov.2000@mail.ru>
2020-09-21 00:00:53 +0300

    Task1 improve and cover with tests

--- src/main/kotlin/ru/itmo/tpo/lab1/task1/Math.kt
+++ src/main/kotlin/ru/itmo/tpo/lab1/task1/Math.kt
@@ -2,48 +2,28 @@ package ru.itmo.tpo.lab1.task1
 
 import java.math.BigDecimal
 import java.math.BigInteger
-import java.math.RoundingMode
 import kotlin.math.abs
-import kotlin.math.pow
-
-fun main() {
-    println(cos(-.1, 0.000001))
-
-    println(cos((-.1).toBigDecimal(), 0.000001.toBigDecimal()))
-    println(cos(1.0.toBigDecimal(), 0.000001.toBigDecimal()))
-    println(cos(10.0.toBigDecimal(), 0.000001.toBigDecimal()))
-    println(cos(100.0.toBigDecimal(), 0.000001.toBigDecimal()))
-    println(cos(1000.0.toBigDecimal(), 0.000001.toBigDecimal()))
-    println(cos(10000.0.toBigDecimal(), 0.000001.toBigDecimal()))
-}
-
-fun Int.sign(): Int = (-1).toDouble().pow(this).toInt()
 
 fun cos(param: BigDecimal, precision: BigDecimal): Double {
     var prevValue: BigDecimal
-    var currentValue = BigDecimal.valueOf(1)
+    var currentValue = BigDecimal.ONE
     var iteration = 0
-    var factor: BigInteger = BigInteger.ONE
+    var factor: BigInteger
     var diff = BigDecimal.valueOf(1)
-    var numerator = BigDecimal.ONE
+    var prevFraction = BigDecimal.ONE
+    val paramSquared = param.pow(2)
 
     while (iteration < MIN_ITERATIONS ||
         (diff.abs() > precision) && iteration < MAX_BIG_ITERATIONS
     ) {
         iteration += 1
-
-        factor *= BigInteger.valueOf(iteration * 2L - 1) * BigInteger.valueOf(iteration * 2L)
-
+        factor = BigInteger.valueOf(iteration * 2L - 1) * BigInteger.valueOf(iteration * 2L)
+        prevFraction = -prevFraction * paramSquared / factor.toBigDecimal()
         prevValue = currentValue
-
-        numerator *= param * param
-        numerator = numerator.negate()
-
-        currentValue = currentValue.add(
-            (numerator.divide(factor.toBigDecimal(), 10, RoundingMode.HALF_UP))
-        )
+        currentValue += prevFraction
         diff = currentValue - prevValue
     }
+
     return currentValue.toDouble()
 }
 
@@ -51,23 +31,22 @@ fun cos(param: Double, precision: Double): Double {
     var prevValue = Double.MIN_VALUE
     var currentValue = 1.0
     var iteration = 0
-    var factor = 1L
+    var prevFraction = 1.0
 
     while (iteration < MIN_ITERATIONS ||
         (abs(currentValue - prevValue) > precision && iteration < MAX_ITERATIONS)
     ) {
         iteration += 1
-        factor *= (iteration * 2 - 1) * (iteration * 2)
         prevValue = currentValue
-        val a = iteration.sign() * param.pow(2 * iteration)
-        currentValue += (a / factor)
+        prevFraction *= -1 * param * param / (iteration * 2 - 1) / (iteration * 2)
+        currentValue += prevFraction
     }
 
     return currentValue
 }
 
-const val MIN_ITERATIONS = 5
-const val MAX_ITERATIONS = 10
+const val MIN_ITERATIONS = 1000
+const val MAX_ITERATIONS = 150000
 const val MAX_BIG_ITERATIONS = 15000
 
 
