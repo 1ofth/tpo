55b4ad9e5236ff3d49d3dbbe9ee6fd3b8a1fb423
1ofth <edem.ibraimov.2000@mail.ru>
2020-10-26 15:32:40 +0300

    Lab2: FunctionTest move test data to csv file

--- src/test/kotlin/ru/ifmo/tpo/lab2/FunctionTest.kt
+++ src/test/kotlin/ru/ifmo/tpo/lab2/FunctionTest.kt
@@ -2,7 +2,7 @@ package ru.ifmo.tpo.lab2
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.CsvFileSource
 import ru.itmo.tpo.lab2.ComplexExpr
 import ru.itmo.tpo.lab2.DEFAULT_PRECISION
 import ru.itmo.tpo.lab2.LogExpr
@@ -12,50 +12,33 @@ class FunctionTest {
     private lateinit var function: ComplexExpr
 
     @ParameterizedTest(name = "full system({0}) = {1}")
-    @CsvSource(
-        "-1.0, 0.82404225", "-2.0, -3.9089476"
-    )
-    fun `all stubbed`(paramReadable: String, param: Double, expectedResult: Double) {
+    @CsvFileSource(resources = ["/negative.csv"])
+    fun `all stubbed`(param: Double, expectedResult: Double) {
         function = ComplexExpr(trigExprStub, logExprStub)
-        testTemplate(paramReadable, param, expectedResult)
+        assertEquals(expectedResult, function.calc(param), DEFAULT_PRECISION)
     }
 
     @ParameterizedTest(name = "full system({0}) = {1}")
-    @CsvSource(
-        "-1.0, 0.82404225", "-2.0, -3.9089476"
-    )
-    fun `log expr stubbed`(paramReadable: String, param: Double, expectedResult: Double) {
+    @CsvFileSource(resources = ["/negative.csv"])
+    fun `log expr stubbed`(param: Double, expectedResult: Double) {
         function = ComplexExpr(TrigonometricExpr(cotStub, cscStub, cosStub, sinStub, secStub), logExprStub)
-        testTemplate(paramReadable, param, expectedResult)
+        assertEquals(expectedResult, function.calc(param), DEFAULT_PRECISION)
     }
 
     @ParameterizedTest(name = "full system({0}) = {1}")
-    @CsvSource(
-        "-1.0, 0.82404225", "-2.0, -3.9089476"
-    )
-    fun `trigonometry expr stubbed`(paramReadable: String, param: Double, expectedResult: Double) {
+    @CsvFileSource(resources = ["/negative.csv"])
+    fun `trigonometry expr stubbed`(param: Double, expectedResult: Double) {
         function = ComplexExpr(trigExprStub, LogExpr(log3Stub, log5Stub, log10Stub))
-        testTemplate(paramReadable, param, expectedResult)
+        assertEquals(expectedResult, function.calc(param), DEFAULT_PRECISION)
     }
 
     @ParameterizedTest(name = "full system({0}) = {1}")
-    @CsvSource(
-        "-1.0, 0.82404225", "-2.0, -3.9089476"
-    )
-    fun `without stubs`(paramReadable: String, param: Double, expectedResult: Double) {
+    @CsvFileSource(resources = ["/negative.csv"])
+    fun `without stubs`(param: Double, expectedResult: Double) {
         function = ComplexExpr(
             TrigonometricExpr(cotStub, cscStub, cosStub, sinStub, secStub),
             LogExpr(log3Stub, log5Stub, log10Stub)
         )
-        testTemplate(paramReadable, param, expectedResult)
+        assertEquals(expectedResult, function.calc(param), DEFAULT_PRECISION)
     }
-
-    private fun testTemplate(paramReadable: String, param: Double, expectedResult: Double) {
-        val actual = function.calc(param)
-        assertEquals(
-            actual - expectedResult, DEFAULT_PRECISION,
-            "function with argument $paramReadable should be equal $expectedResult, but was $actual"
-        )
-    }
-
 }
